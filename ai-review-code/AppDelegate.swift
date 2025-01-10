7c62a0df0c4bf4be28151d9e79ef1e00feaa6dbd
vu.dinh <vudv.2508@gmail.com>
2025-01-11 01:01:01 +0700

    aaaa

--- ai-review-code/AppDelegate.swift
+++ ai-review-code/AppDelegate.swift
@@ -31,5 +31,37 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
+private func prepareObjecMessageSent(_ object: MesMessageSent) -> MesMessageSent {
+        if let attachMent = object.message?.attachment {
+            let atk = AttachedFile()
+            atk.fileId = attachMent.fileId
+            atk.name = attachMent.name
+            atk.size = attachMent.size
+            atk.date = Date(timeIntervalSince1970: (attachMent.created ?? 0) / 1000)
+            atk.mimeType = MimeType.convert(rawValue: attachMent.mimeType)
+            atk.officeId = attachMent.officeId
+            atk.sendToMedicalStaff = attachMent.sendToMedicalStaff ?? false
+            self.listAttachments.append(atk)
+        }
+        
+        if (ChatConstants.officeUserIdChatBot == object.message?.officeUserId) {
+            object.message?.displayName = "AP2004_AT_CHAT_BOT".localized
+        } else {
+            let sentUser = self.listMembers.first(where: {
+                $0.id == object.message?.officeUserId
+            })
+            if (sentUser != nil) {
+                object.message?.displayName = sentUser?.displayName ?? ""
+                object.message?.accountStatus = sentUser?.accountStatus ?? 2
+                if (!(sentUser?.isInvalid())!) {
+                    object.message?.officeId = sentUser?.officeId ?? ""
+                }
+            }
+        }
+        if (object.message?.officeUserId != self.presenter.currentOfficeUserId) {
+            object.message?.read = false
+        }
+        object.message?.checkMedicalStaff(self.socketPresenter.medicalStaff)
+        
+}
 
--- ai-review-code/SceneDelegate.swift
+++ ai-review-code/SceneDelegate.swift
@@ -47,4 +47,38 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // to restore the scene back to its current state.
     }
 
+    private func prepareObjecMessageSent(_ object: MesMessageSent) -> MesMessageSent {
+        if let attachMent = object.message?.attachment {
+            let atk = AttachedFile()
+            atk.fileId = attachMent.fileId
+            atk.name = attachMent.name
+            atk.size = attachMent.size
+            atk.date = Date(timeIntervalSince1970: (attachMent.created ?? 0) / 1000)
+            atk.mimeType = MimeType.convert(rawValue: attachMent.mimeType)
+            atk.officeId = attachMent.officeId
+            atk.sendToMedicalStaff = attachMent.sendToMedicalStaff ?? false
+            self.listAttachments.append(atk)
+        }
+        
+        if (ChatConstants.officeUserIdChatBot == object.message?.officeUserId) {
+            object.message?.displayName = "AP2004_AT_CHAT_BOT".localized
+        } else {
+            let sentUser = self.listMembers.first(where: {
+                $0.id == object.message?.officeUserId
+            })
+            if (sentUser != nil) {
+                object.message?.displayName = sentUser?.displayName ?? ""
+                object.message?.accountStatus = sentUser?.accountStatus ?? 2
+                if (!(sentUser?.isInvalid())!) {
+                    object.message?.officeId = sentUser?.officeId ?? ""
+                }
+            }
+        }
+        if (object.message?.officeUserId != self.presenter.currentOfficeUserId) {
+            object.message?.read = false
+        }
+        object.message?.checkMedicalStaff(self.socketPresenter.medicalStaff)
+        
+}
+
 

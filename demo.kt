f1051e6413a049f0f5afb62360a7aa5f1663c385
vu.dinh <vudv.2508@gmail.com>
2025-01-11 10:24:53 +0700

    aa

--- demo.kt
+++ demo.kt
@@ -1,7 +1,7 @@
 import java.util.Scanner
 
 fun main(args: Array<String>) {
-    val reader = Scanner(System.`in`)
+    val reader11111 = Scanner(System.`in`)
     print("Enter a number: ")
 
     var integer:Int = reader.nextInt()

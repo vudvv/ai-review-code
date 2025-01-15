77f5021f81a092ab01db860f25c33294d67cea3f
vu.dinh <vudv.2508@gmail.com>
2025-01-15 21:43:28 +0700

    x9

--- demo.swift
+++ demo.swift
@@ -69,4 +69,11 @@ func sum() {
         let c = a + b
         let d = a + b + c
     }
+
+     func sum() {
+         let a = 0
+        let b = 1
+        let c = a + b
+        let d = a + b + c
+    }
 }

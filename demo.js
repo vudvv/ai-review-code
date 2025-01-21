63e7433e6a3c4cd9570212f4b42c7e58adb33c98
vu.dinh <vudv.2508@gmail.com>
2025-01-21 17:53:49 +0700

    a3

--- demo.js
+++ demo.js
@@ -16,3 +16,12 @@ const sum = num1 + num2;
 
 // display the sum
 console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
+
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);

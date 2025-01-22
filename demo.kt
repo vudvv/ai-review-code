6b9301b0ce72ac66a1538e31822e84fd77f1f1db
vu.dinh <vudv.2508@gmail.com>
2025-01-22 10:54:10 +0700

    a1

--- demo.kt
+++ demo.kt
@@ -29,3 +29,12 @@ func sum() {
    let c = a + b
    let d = a + b + c
 }
+
+fun main(args: Array<String>) {
+    val reader11111 = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    var integer:Int = reader.nextInt()
+
+    println("You entered: $integer")
+}

7846dc4231d81bdbbcbe67bbb60529248896d21d
vu.dinh <vudv.2508@gmail.com>
2025-01-22 10:35:33 +0700

    a5

--- demo.swift
+++ demo.swift
@@ -9,6 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+fun main(args: Array<String>) {
+    val reader11111 = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    var integer:Int = reader.nextInt()
+
+    println("You entered: $integer")
+}
+
     func sum() {
          let a = 0
         let b = 1
@@ -76,4 +85,11 @@ func sum() {
         let c = a + b
         let d = a + b + c
     }
+}
+
+func sum() {
+    let a = 0
+   let b = 1
+   let c = a + b
+   let d = a + b + c
 }

be7b1f3a59fcfa66c49a16bf7e936f5fde53fbcc
vu.dinh <vudv.2508@gmail.com>
2025-01-23 08:13:55 +0700

    #181208: Implement a function ME0005

--- demo.kt
+++ demo.kt
@@ -47,3 +47,12 @@ fun main(args: Array<String>) {
 
     println("You entered: $integer")
 }
+
+fun main(args: Array<String>) {
+    val reader11111 = Scanner(System.`in`)
+    print("Enter a number: ")
+
+    var integer:Int = reader.nextInt()
+
+    println("You entered: $integer")
+}
